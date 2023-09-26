437e7bc44a31fde6fff02750fb347161221408bb
Saksham Mittal <gotlouemail@gmail.com>
2023-09-26 15:42:13 +0530

    feat: add tor-browser.nix

--- modules/modules.nix
+++ modules/modules.nix
@@ -209,6 +209,7 @@ let
     ./programs/tmate.nix
     ./programs/tmux.nix
     ./programs/topgrade.nix
+    ./programs/tor-browser.nix
     ./programs/translate-shell.nix
     ./programs/urxvt.nix
     ./programs/vdirsyncer.nix
